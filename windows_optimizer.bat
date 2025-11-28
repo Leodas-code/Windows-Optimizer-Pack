1d2cb8821c488e8602bfbf816857e81c6694fe5a
M.Rithish <annalakshmi1507@gmail.com>
2025-11-28 21:11:17 +0530

    Add files via upload

---
+++ windows_optimizer.bat
@@ -0,0 +1,14 @@
+reg add "HKCU\Control Panel\Desktop\WindowMetrics" /v MinAnimate /t REG_SZ /d 0 /f
+
+taskkill /f /im explorer.exe
+start explorer.exe
+
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" /v LetAppsRunInBackground /t REG_DWORD /d 2 /f
+
+del "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\*" /q
+
+powercfg -s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+
+del /q /f "%TEMP%\*"
+
+echo Created by Rithish (Age 14)
